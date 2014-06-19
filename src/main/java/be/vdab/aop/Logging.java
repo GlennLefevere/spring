14e8c7b1fc861258863aeb1bfbebb1200b4dd447
glenn <glennlefevere@hotmail.com>
2014-06-19 11:45:50 +0200

    met twitter

--- src/main/java/be/vdab/aop/Logging.java
+++ src/main/java/be/vdab/aop/Logging.java
@@ -1,25 +1,23 @@
 package be.vdab.aop;
 
-import java.util.Collection;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
-import org.jboss.logging.Logger;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(1)
-class Auditing {
-	private final static Logger logger = Logger.getLogger(Auditing.class.getName());
+class Logging {
+	private final static Logger logger = Logger.getLogger(Logging.class.getName());
 
-	@AfterReturning(pointcut = "be.vdab.aop.PointcutExpressions.services()", returning = "returnValue")
-	void schrijfAudit(JoinPoint joinPoint, Object returnValue) {
+	@AfterThrowing(pointcut = "be.vdab.aop.PointcutExpressions.servicesEnTransacties()", throwing = "ex")
+	void schrijfException(JoinPoint joinPoint, Throwable ex) {
 		StringBuilder builder = new StringBuilder("\nTijdstip\t").append(new Date());
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		if (authentication != null) {
@@ -30,14 +28,6 @@ class Auditing {
 		for (Object object : joinPoint.getArgs()) {
 			builder.append("\nParameter ").append(++parameterVolgnummer).append('\t').append(object);
 		}
-		if (returnValue != null) {
-			builder.append("\nReturn\t\t");
-			if (returnValue instanceof Collection) { 
-			builder.append(((Collection<?>) returnValue).size()).append(" objectss");
-			} else {
-			builder.append(returnValue.toString());
-			}
-		}
-		logger.info(builder.toString());
+		logger.log(Level.SEVERE, builder.toString(), ex);
 	}
 }
